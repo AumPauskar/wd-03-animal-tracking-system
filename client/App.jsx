4c180cf0478fd09bdf84d6647fc648334ffe6d84
pratham704 <pratham.siddannavar28@gmail.com>
2024-06-04 19:38:28 +0530

    test

--- client/App.jsx
+++ client/App.jsx
@@ -1,28 +1,22 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { registerForPushNotificationsAsync } from 'expo-notifications';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import PublicRouters from "./Navigation/PublicRouters";
 
-const App = () => {
-  useEffect(() => {
-    (async () => {
-      const token = await registerForPushNotificationsAsync();
-      console.log('Expo Push Token:', token.data);
-    })();
-  }, []);
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Check the console for Expo Push Token.</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor="black" translucent={false} style="dark" />
+      <View style={styles.container}>
+        <PublicRouters />
+      </View>
+    </>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
-
-export default App;
